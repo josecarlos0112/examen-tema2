1d414317138e55e9e1efe3bcfd488a492b741fc1
josecarlos0112 <jzorrgar@myuax.com>
2023-12-14 12:45:59 +0100

    update

---
+++ parte2/estudiante/cmake-build-debug/CMakeFiles/estudiante.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/estudiante.dir/estudiante.cpp.obj"
+  "CMakeFiles/estudiante.dir/estudiante.cpp.obj.d"
+  "CMakeFiles/estudiante.dir/main.cpp.obj"
+  "CMakeFiles/estudiante.dir/main.cpp.obj.d"
+  "estudiante.exe"
+  "estudiante.exe.manifest"
+  "estudiante.pdb"
+  "libestudiante.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/estudiante.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ parte2/estudiante/cmake-build-debug/CMakeFiles/estudiante.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for estudiante.
+# This may be replaced when dependencies are built.
