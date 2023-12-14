ac0df5956ffbecd7e54e0550b9d5d1c2f7171831
josecarlos0112 <jzorrgar@myuax.com>
2023-12-14 12:25:36 +0100

    creacion de estructura estudiante

--- parte2/estudiante/main.cpp
+++ parte2/estudiante/main.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <string>
 
+// Estructura Estudiante en C++
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<std::string> materias;  // Para la gestión de materias
+};
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
