15b0be6633358b946a41ef17390bf916009cb6d1
josecarlos0112 <jzorrgar@myuax.com>
2023-12-14 12:26:52 +0100

    correccion de archivo e inclusion de funcion void

--- parte2/estudiante/estudiante.cpp
+++ parte2/estudiante/estudiante.cpp
@@ -3,3 +3,26 @@
 //
 
 #include "estudiante.h"
+#include <vector>
+#include <string>
+
+// Estructura Estudiante en C++
+struct Estudiante {
+    std::string nombre;
+    int edad;
+    float promedio;
+    std::vector<std::string> materias;  // Para la gestión de materias
+};
+
+// Función para mostrar un Estudiante
+void mostrarEstudiante(const Estudiante& estudiante) {
+    std::cout << "Nombre: " << estudiante.nombre << std::endl;
+    std::cout << "Edad: " << estudiante.edad << std::endl;
+    std::cout << "Promedio: " << estudiante.promedio << std::endl;
+
+    // Mostrar materias
+    std::cout << "Materias inscritas:" << std::endl;
+    for (const auto& materia : estudiante.materias) {
+        std::cout << "- " << materia << std::endl;
+    }
+}
